b7ec53d567effffcf11c28a85e2903397f74f05d
justinsteinberg <justin.steinberg1@aol.com>
2023-12-04 23:37:23 -0500

    Not sure why I'm getting a CORS error. All the ENV variables line up.

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL
   }
  ));
 const sessionOptions = {
     secret: "any string",
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
 };
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
